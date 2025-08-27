19bbb7ca3049b868389775ce1f26993cad799888
sprucceboton4664 <ezambranasoreta@gmail.com>
2025-08-26 21:02:20 -0400

    comit2

---
+++ razo2.1/Data/TareaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using razo2._1.Models;
+namespace razo2._1.Data
+{
+    public class TareaDBContex : DbContext
+    {
+        public TareaDBContex(DbContextOptions<TareaDBContex> options) : base(options)
+        {
+            
+        }
+        public DbSet<Tarea> Tareas { get; set; } 1
+        protected TareaDBContex()
+        {
+
+        }
+    }
+}
---
+++ razo2.1/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace razo2._1.Models
+{
+    public class Tarea
+    {
+        public int IdTarea { get; set; }
+        public string NombreTarea { get; set; }
+        public DateTime FechaVencimiento { get; set; }    
+        public string estado { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
