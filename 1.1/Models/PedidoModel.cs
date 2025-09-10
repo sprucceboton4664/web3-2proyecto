3444371f5a035afac9d386176bd3878a1e1eb5ac
sprucceboton4664 <ezambranasoreta@gmail.com>
2025-09-09 21:02:30 -0400

    comit 09//09/2025 2.3

--- 1.1/Models/PedidoModel.cs
+++ 1.1/Models/PedidoModel.cs
@@ -1,6 +1,25 @@
-﻿namespace _1._1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAMVC.Models
 {
     public class PedidoModel
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La fecha del pedido es obligatoria.")]
+        public DateTime FechaPedido { get; set; }
+
+        [Required(ErrorMessage = "El cliente es obligatorio.")]
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "La dirección de envío es obligatoria.")]
+        [StringLength(200, ErrorMessage = "La dirección no puede tener más de 200 caracteres.")]
+        public string Direccion { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto total debe ser mayor que 0.")]
+        public decimal MontoTotal { get; set; }
+
+        public ClienteModel Cliente { get; set; }
+        public ICollection<DetallePedidoModel> DetallePedidos { get; set; }
     }
 }
