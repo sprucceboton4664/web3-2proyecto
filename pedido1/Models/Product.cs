2b01485edcee0a28dd6f283a39d32508f6362d67
sprucceboton4664 <ezambranasoreta@gmail.com>
2025-09-24 18:31:59 -0400

    comit 13 aqui anadi Agregaremos la propiedad Descripción y validaciones para precio y stock

--- pedido1/Models/Product.cs
+++ pedido1/Models/Product.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace pedido1.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Nombre { get; set; }
+
+        [StringLength(255)]
+        public string Descripcion { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser positivo.")]
         public decimal Precio { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El stock debe ser mayor o igual a 0.")]
         public int Stock { get; set; }
 
         public ICollection<OrderItem> DetallePedidos { get; set; }
